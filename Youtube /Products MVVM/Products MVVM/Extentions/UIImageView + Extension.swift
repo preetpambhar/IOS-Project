23dfc735a07e25c7841c23b7d7d7e8406870c225
Preet Pambhar <preetpambhar73530@gmail.com>
2024-05-04 03:07:41 -0400

    feat: get api, data fetching

---
+++ Youtube /Products MVVM/Products MVVM/Extentions/UIImageView + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageView + Extension.swift
+//  Products MVVM
+//
+//  Created by Preet Pambhar on 2024-05-04.
+//
+
+import Foundation
---
+++ Youtube /Products MVVM/Products MVVM/Screens/Product/View/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  Products MVVM
+//
+//  Created by Preet Pambhar on 2024-05-04.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
